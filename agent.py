dd46906ec49f560d2b4061f22e2c4c8499e7f7cb
devrajn <nik8470@gmail.com>
2021-05-20 20:31:11 -0400

    testing with some boilerplate because impl was slightly messed up

--- agent.py
+++ agent.py
@@ -1,33 +1,115 @@
 """
 Single agent that interacts with central learner and learns its own parameters.
+Informed by https://github.com/Kaixhin/Rainbow/blob/master/agent.py
 """
 import os
 import torch
 import torch.nn as nn
 from collections import deque 
+from dqn import DQN
+
 
 class Agent:
     def __init__(self, args, action_space, model, central_rep):
         self.args = args
         self.model = model # ex. PPO, but starting simple with DQN
+        self.online_net = self.model
         self.central_rep = central_rep
         self.optimizer = optim.Adam(self.model.parameters(), lr=args.learning_rate, eps=args.adam_eps)
+        self.batch_size = args.batch_size # is there a different batch size between them?
+        self.Vmin = args.V_min
+        self.Vmax = args.V_max
+        self.atoms = args.atoms
+        self.support = torch.linspace(args.V_min, args.V_max, self.atoms).to(device=args.device)  # Support (range) of z
+        self.delta_z = (args.V_max - args.V_min) / (self.atoms - 1)
 
-    def action(self, state):
-        return torch.randint(len(self.action_space), size=(1,)).long()
+        self.online_net.train()
 
-    def update_params(self):
+        self.target_net = DQN(args, self.action_space).to(device=args.device)
+        self.update_target_net()
+        self.target_net.train()
+        for param in self.target_net.parameters():
+            param.requires_grad = False
+
+    # DQN parameter update
+    def update_params(self, memory):
+        idxs, states, actions, returns, next_states, nonterminals, weights = memory.sample(self.batch_size)
+        log_ps, _ = self.model(states, log=True)  # Log probabilities log p(s_t, ·; θonline)
+        log_ps_a = log_ps[range(self.batch_size), actions]  # log p(s_t, a_t; θonline)
+
+        with torch.no_grad():
+            # Calculate nth next state probabilities
+            pns = self.model(next_states) # Probabilities p(s_t+n, ·; θonline)
+            dns = self.support.expand_as(pns) * pns # Distribution d_t+n = (z, p(s_t+n, ·; θonline))
+            argmax_indices_ns = dns.sum(2).argmax(1) # Perform argmax action selection using online network: argmax_a[(z, p(s_t+n, a; θonline))]
+            pns = self.target_net(next_states)  # Probabilities p(s_t+n, ·; θtarget)
+            pns_a = pns[range(self.batch_size), argmax_indices_ns]  # Double-Q probabilities p(s_t+n, argmax_a[(z, p(s_t+n, a; θonline))]; θtarget)
+            # Compute Tz (Bellman operator T applied to z)
+            Tz = returns.unsqueeze(1) + nonterminals * (self.discount ** self.n) * self.support.unsqueeze(0)  # Tz = R^n + (γ^n)z (accounting for terminal states)
+            Tz = Tz.clamp(min=self.Vmin, max=self.Vmax)  # Clamp between supported values
+            # Compute L2 projection of Tz onto fixed support z
+            b = (Tz - self.Vmin) / self.delta_z  # b = (Tz - Vmin) / Δz
+            l, u = b.floor().to(torch.int64), b.ceil().to(torch.int64)
+            # Fix disappearing probability mass when l = b = u (b is int)
+            l[(u > 0) * (l == u)] -= 1
+            u[(l < (self.atoms - 1)) * (l == u)] += 1
+
+            # Distribute probability of Tz
+            m = states.new_zeros(self.batch_size, self.atoms)
+            offset = torch.linspace(0, ((self.batch_size - 1) * self.atoms), self.batch_size).unsqueeze(1).expand(self.batch_size, self.atoms).to(actions)
+            m.view(-1).index_add_(0, (l + offset).view(-1), (pns_a * (u.float() - b)).view(-1))  # m_l = m_l + p(s_t+n, a*)(u - b)
+            m.view(-1).index_add_(0, (u + offset).view(-1), (pns_a * (b - l.float())).view(-1))  # m_u = m_u + p(s_t+n, a*)(b - l)
+
+
+        loss = -torch.sum(m * log_ps_a, 1)  # Cross-entropy loss (minimises DKL(m||p(s_t, a_t)))
+        self.online_net.zero_grad()
+        (weights * loss).mean().backward()  # Backpropagate importance-weighted minibatch loss
+        clip_grad_norm_(self.online_net.parameters(), self.norm_clip)  # Clip gradients by L2 norm
+        self.optimiser.step()
+
+        memory.update_priorities(idxs, loss.detach().cpu().numpy())  # Update priorities of sampled transitions
+        
         # learn for a specific agent is called during training
         # we want to use the CPC loss function as part of training for this model
         # but the CPC is centralized; so we somehow need to share parameter updates in this central model???
-        log_loss = self.model()
+        # log_loss = self.model()
         
-        nce_loss = self.central_rep.get_loss()
-        loss = nce_loss * nce_weight + log_loss * log_weight
+        # nce_loss = self.central_rep.get_loss()
+        # loss = nce_loss * nce_weight + log_loss * log_weight
         
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        # optimizer.zero_grad()
+        # loss.backward()
+        # optimizer.step()
+
+    def reset_noise(self):
+        self.online_net.reset_noise()
+
+    # Acts based on single state (no batch)
+    def act(self, state):
+        with torch.no_grad():
+            return (self.online_net(state.unsqueeze(0)) * self.support).sum(2).argmax(1).item()
+
+    # Acts with an ε-greedy policy (used for evaluation only)
+    def act_e_greedy(self, state, epsilon=0.001):  # High ε can reduce evaluation scores drastically
+        return np.random.randint(0, self.action_space) if np.random.random() < epsilon else self.act(state)
+
+    def update_target_net(self):
+        self.target_net.load_state_dict(self.online_net.state_dict())
+
+    # Save model parameters on current device (don't move model between devices)
+    def save(self, path, name='model.pth'):
+      torch.save(self.online_net.state_dict(), os.path.join(path, name))
+
+    # Evaluates Q-value based on single state (no batch)
+    def evaluate_q(self, state):
+      with torch.no_grad():
+        return (self.online_net(state.unsqueeze(0)) * self.support).sum(2).max(1)[0].item()
+
+    def train(self):
+      self.online_net.train()
+
+    def eval(self):
+      self.online_net.eval()
         
 def set_mode(models, mode="train"):
     if mode == "train":
@@ -52,4 +134,3 @@ class ObsBuffer:
     def drain(self):
         while self.buf:
             self.buf.popleft()
-    
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from pettingzoo.butterfly import cooperative_pong_v2
 from ppo import PPO
 from dqn import DQN
 from stable_baselines3.ppo import CnnPolicy
-from agent import Agent, ObsBuffer, set_mode
+from agent import Agent, ObsBuffer, set_mod
+from memory import ReplayMemory
 
 def main():
     seed = torch.randint(10000)
@@ -18,7 +19,58 @@ def main():
                         type=str,
                         default=None,
                         help="PettingZoo environment to use. Choose between `pistonball` and `pong`.")
-    # TODO: Add all the running arguments
+    parser.add_argument("--num-timesteps",
+                        type=int,
+                        default=12,
+                        help="Number of timesteps to consider for central model")
+    parser.add_argument("--batch-size",
+                        type=int,
+                        default=8,
+                        help="Batch size for central model training")
+    parser.add_argument("--adam-eps",
+                        type=float,
+                        default=1.5e-4,
+                        help="Adam epsilon") 
+    parser.add_argument("--seq-len",
+                        type=str,
+                        default=20480,
+                        help="Total sequence length for central model")
+    parser.add_argument("--buffer-size",
+                        type=int,
+                        default=int(1e5),
+                        help="Experience replay memory capacity")
+    parser.add_argument("--max-horizon",
+                        type=int,
+                        default=int(2e5),
+                        help="Number of training steps (4x number of frames)")
+    parser.add_argument("--learn-start",
+                        type=int,
+                        default=int(1000),
+                        help="Number of steps before starting training")
+    parser.add_argument("--priority-weight",
+                        type=float,
+                        default=0.4,
+                        help="Initial prioritised experience replay importance sampling weight")
+    parser.add_argument("--eval-buffer-size",
+                        type=int,
+                        default=500,
+                        help="number of evaluations for validation of update parameters")
+    parser.add_argument("--update-period",
+                        type=int,
+                        default=1,
+                        help="Frequency with which to update parameters")
+    parser.add_argument("--multi-step",
+                        type=int,
+                        default=20,
+                        help="Number of steps for multi-step return")
+    parser.add_argument("--V-min",
+                        type=float,
+                        default=-10,
+                        help="Minimum of value distribution support")
+    parser.add_argument("--V-max",
+                        type=float,
+                        default=10,
+                        help="Maximum of value distribution support")
 
     env = None
     if args.env == 'pistonball':
@@ -53,8 +105,8 @@ def main():
     memory = {} # execution memory
     val_memory = {} # validation memory
 
-    central_rep = CPC(args.num_timesteps, args.batch_size, args.seq_len) # TODO: add to args
-    
+    central_rep = CPC(args.num_timesteps, args.batch_size, args.seq_len)
+
     for agent in env.agents:
         # model = PPO(CnnPolicy,
         #             env,
@@ -71,12 +123,39 @@ def main():
         #             clip_range_vf=1) # TODO: check and fix
         model = DQN(args, env.action_spaces[agent])
         models[agent] = Agent(args, env.action_spaces[agent].n, model, central_rep)
+        
+        memory[agent] = ReplayMemory(args, args.buffer_size)
+
+        priority_weight_increase = (1 - args.priority_weight) / (
+            args.max_horizon - args.learn_start
+        )
+
+        # Construct validation memory
+        val_memory[agent] = ReplayMemory(args, args.eval_buffer_size)
+
+
+    # prefill buffer before training
+    t, converged = 0, False
+    while not converged:
+        i = 0
+        env.reset()
+        for agent in env.agent_iter(args.eval_buffer_size):
+            if i % len(env.agents):
+                t += 1
+                if t > args.eval_buffer_size:
+                    converged=True
+                    break
+            observation, reward, done, info = env.last()
+            action = np.random.randint(0,env.action_spaces[agent].n) if not done else None
+            env.step(action)
+            val_memory[agent].append(torch.tensor(observation), None, None, done)
+            i += 1
 
     # Training
     set_mode(models)
     t, converged = 0, False
-    progress_bar = tqdm(total=args.max_horizon) # TODO: add to args
-    obs_buffer = ObsBuffer(self.num_timesteps) # num_timesteps
+    progress_bar = tqdm(total=args.max_horizon)
+    obs_buffer = ObsBuffer(args.num_timesteps)
     while not converged:
         env.reset()
         i = 0
@@ -93,16 +172,21 @@ def main():
 
             action = models[agent].act(torch.tensor(observation))
             if not done:
-                env.step(action)  # Step
+                env.step(action)
             else:
                 env.step(None)
 
-            if i % args.update_period: # TODO: add update_period to args
-                obs_buffer.append(observation)
-                # Train
-                models[agent].update_params()
-                # Train central representation
-                central_rep.update_params(observation_buffer)
+            memory[agent].append(torch.tensor(observation), action, reward, done)
+
+            if t >= args.learn_start:
+                memory[agent].priority_weight = min(memory[agent].priority_weight + priority_weight_increase, 1)
+
+                if i % args.update_period:
+                    obs_buffer.append(observation)
+                    # Train individual agent
+                    models[agent].update_params(memory[agent], central_rep.get_loss())
+                    # Train central representation
+                    central_rep.update_params(observation_buffer)
 
 
 if __name__ == '__main__':
